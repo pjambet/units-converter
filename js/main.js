8893e0145dd5a03a20bc907503e71ae7e52724b8
pjambet <pierre.jambet@gmail.com>
2014-10-04 12:47:31 -0400

    Refactor event listeners

--- js/main.js
+++ js/main.js
@@ -6,72 +6,63 @@
       $speedMetric = document.getElementById('speed-metric'),
       $speedImperial = document.getElementById('speed-imperial'),
       $finishTime = document.getElementById('finish-time'),
-      distanceInMeters = null,
-      distanceInMiles = null,
-      finishTimeInSeconds = null,
-      kilometersToMilesRatio = 1.60934;
+      kilometersToMilesRatio = 1.60934,
+      dataObject = {
+        distanceInMeters: null,
+        finishTimeInSeconds: null,
+      };
 
   $distanceMetric.addEventListener('input', function(event) {
-    distanceInMiles = this.value / kilometersToMilesRatio;
-    $distanceImperial.value = distanceInMiles;
-    distanceInMeters = this.value * 1000;
-    calculatePace();
-    calculateSpeed();
+    dataObject.distanceInMeters = this.value * 1000;
   });
 
   $distanceImperial.addEventListener('input', function(event) {
-    distanceInMeters = this.value * kilometersToMilesRatio;
-    $distanceMetric.value = distanceInMeters;
-    distanceInMeters = distanceInMeters * 1000;
-    calculatePace();
-    calculateSpeed();
+    dataObject.distanceInMeters = this.value * kilometersToMilesRatio * 1000;
   });
 
   $finishTime.addEventListener('input', function(event) {
-    finishTimeInSeconds = 0;
+    dataObject.finishTimeInSeconds = 0;
     // Trash this and use Date.parse
     this.value.split(':').forEach(function(split, i) {
       if (i === 0) {
         // Hours
-        finishTimeInSeconds += parseInt(split) * 60 * 60;
+        dataObject.finishTimeInSeconds += parseInt(split) * 60 * 60;
       } else if (i === 1) {
         // Minutes
-        finishTimeInSeconds += parseInt(split) * 60;
+        dataObject.finishTimeInSeconds += parseInt(split) * 60;
       } else if (i === 2) {
         // Seconds
-        finishTimeInSeconds += parseInt(split);
+        dataObject.finishTimeInSeconds += parseInt(split);
       }
     });
-    calculatePace();
-    calculateSpeed();
   });
 
   var calculatePace = function() {
-    if (!distanceInMeters || !finishTimeInSeconds) {
+    if (!dataObject.distanceInMeters || !dataObject.finishTimeInSeconds) {
       return;
     }
 
     // 10 km, 1h => 6 min / km
     // 10 km, 1h => 360 s / 1000m
     // 10000m, 3600s
-    var metricPace = Math.round(finishTimeInSeconds / distanceInMeters * 1000 / 60 * 100) / 100;
-    var imperialPace = Math.round(finishTimeInSeconds / distanceInMeters * 1000 / 60 * 100) / 100 * kilometersToMilesRatio;
-    $paceMetric.innerHTML = convertDecimalToTime(metricPace);
-    $paceImperial.innerHTML = convertDecimalToTime(imperialPace);
+    var metricPace = Math.round(dataObject.finishTimeInSeconds / dataObject.distanceInMeters * 1000 / 60 * 100) / 100;
+    var imperialPace = Math.round(dataObject.finishTimeInSeconds / dataObject.distanceInMeters * 1000 / 60 * 100) / 100 * kilometersToMilesRatio;
+    $paceMetric.value = convertDecimalToTime(metricPace);
+    $paceImperial.value = convertDecimalToTime(imperialPace);
   };
 
   var calculateSpeed = function() {
-    if (!distanceInMeters || !finishTimeInSeconds) {
+    if (!dataObject.distanceInMeters || !dataObject.finishTimeInSeconds) {
       return;
     }
 
     // 10 km, 1h => 10 km / h
     // 10 km, 1h => 1000m / 360s
     // 10000m, 3600s
-    var metricSpeed = Math.round((distanceInMeters / 1000) / (finishTimeInSeconds / 3600));
-    var imperialSpeed = Math.round((distanceInMeters * kilometersToMilesRatio / 1000) / (finishTimeInSeconds / 3600));
-    $speedMetric.innerHTML = metricSpeed;
-    $speedImperial.innerHTML = imperialSpeed;
+    var metricSpeed = Math.round((dataObject.distanceInMeters / 1000) / (dataObject.finishTimeInSeconds / 3600));
+    var imperialSpeed = Math.round((dataObject.distanceInMeters * kilometersToMilesRatio / 1000) / (dataObject.finishTimeInSeconds / 3600));
+    $speedMetric.value = metricSpeed;
+    $speedImperial.value = imperialSpeed;
   };
 
   var convertDecimalToTime = function(decimalValue) {
@@ -91,4 +82,19 @@
     }
     return time + compoments.join(':');
   };
+
+  Object.observe(dataObject, function(changes){
+
+    changes.forEach(function(change) {
+      if (change.type === 'update' && (change.name === 'finishTimeInSeconds' || change.name === 'distanceInMeters')) {
+        calculatePace();
+        calculateSpeed();
+      }
+      if (change.type === 'update' && (change.name === 'distanceInMeters')) {
+        $distanceMetric.value = Math.round((dataObject.distanceInMeters / 1000) * 100) / 100;
+        $distanceImperial.value = Math.round((dataObject.distanceInMeters / 1000 / kilometersToMilesRatio) * 100) / 100;
+      }
+    });
+
+  });
 })()
